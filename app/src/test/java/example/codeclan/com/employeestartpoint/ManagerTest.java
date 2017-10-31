f1bd8e52636e96c4fe9708172f2f7211a0f85d53
Chris Marshall <cmarshall10450@gmail.com>
2017-10-31 16:42:28 +0000

    Test managerHasDeptName padding

--- app/src/test/java/example/codeclan/com/employeestartpoint/ManagerTest.java
+++ app/src/test/java/example/codeclan/com/employeestartpoint/ManagerTest.java
@@ -3,6 +3,8 @@ package example.codeclan.com.employeestartpoint;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by chris on 31/10/2017.
  */
@@ -16,6 +18,9 @@ public class ManagerTest {
     manager = new Manager(1, "Wilma", "AB123456C", 30000.00, "IT");
   }
 
-  
+  @Test
+  public void managerHasDeptName() {
+    assertEquals("IT", manager.getDeptName());
+  }
 
 }
