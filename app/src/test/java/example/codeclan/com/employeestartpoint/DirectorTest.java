10705d9d310a1ce215a556bd56641cf54850976b
Chris Marshall <cmarshall10450@gmail.com>
2017-10-31 16:54:29 +0000

    Added tests for raiseSalary and setName to Director - all passing

--- app/src/test/java/example/codeclan/com/employeestartpoint/DirectorTest.java
+++ app/src/test/java/example/codeclan/com/employeestartpoint/DirectorTest.java
@@ -1,5 +1,6 @@
 package example.codeclan.com.employeestartpoint;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -10,10 +11,29 @@ import static org.junit.Assert.assertEquals;
 
 public class DirectorTest {
 
+  Director director;
+
+
+  @Before
+  public void before() {
+    director = new Director(1, "Wilma", "AB123456C", 30000.00, "IT", 200000.00);
+  }
+
   @Test
   public void directorHasBudget() {
-    Director director = new Director(1, "Wilma", "AB123456C", 30000.00, "IT", 200000.00);
     assertEquals(200000.00, director.getBudget(), 0.01);
+  }
+
+  @Test
+  public void canRaiseSalary() {
+    director.raiseSalary(1000.0);
+    assertEquals(31000.00, director.getSalary(), 0.01);
+  }
+
+  @Test
+  public void canSetEmployeeName() {
+    director.setName("Betty");
+    assertEquals("Betty", director.getName());
   }
 
 }
